739076d399f5dd8790f2ee07405efac7fd37eacd
Sucrose <pinn.project@outlook.com>
2026-02-05 07:27:10 +0700

    Update: address used

--- src/libs/actions/transaction/index.ts
+++ src/libs/actions/transaction/index.ts
@@ -1,11 +1,11 @@
 'use server'
 
+import { and, eq, inArray } from 'drizzle-orm'
 import { cacheLife, cacheTag } from 'next/cache'
-import { cookies } from 'next/headers'
 import { pick } from 'ramda'
 
-import { useAuth } from '@/libs/actions/auth'
 import { RPCClient } from '@/libs/bitcoin/rpc'
+import { db, schema } from '@/libs/drizzle'
 import { createPagination } from '@/libs/utils'
 
 import { formatter } from './helpers'
@@ -14,20 +14,48 @@ import { type QueryValidator } from './validator'
 export type Transaction = Awaited<ReturnType<typeof formatter>>
 
 export async function findTransactions(accountId: string, query: QueryValidator) {
-  // 'use cache'
-  // cacheTag('transactions')
-  // cacheLife('seconds')
+  'use cache'
+  cacheTag('transactions')
+  cacheLife('seconds')
 
   try {
-    const cookieStore = await cookies()
-    await useAuth(cookieStore)
-
     const rpcClient = new RPCClient()
     await rpcClient.setWallet(accountId)
 
     const wallet = await rpcClient.getWallet()
     const transactions = await rpcClient.listTransactions('*', query.take, (query.page - 1) * query.take)
     const formatted = await Promise.all(transactions.map((tx) => formatter(rpcClient, tx)))
+
+    // Update the address to "used" based on a transaction that has a "confirmed" status.
+    if (formatted.length > 0) {
+      const CHUNK_SIZE = 100
+      const addrs = Array.from(
+        new Set(
+          formatted
+            .filter((tx) => tx.status === 'confirmed')
+            .flatMap((tx) => [...tx.inputs, ...tx.outputs].map((r) => r.address))
+        )
+      )
+
+      await db.transaction(async (tx) => {
+        for (let i = 0; i < addrs.length; i += CHUNK_SIZE) {
+          const chunk = addrs.slice(i, i + CHUNK_SIZE)
+
+          await tx
+            .update(schema.addresses)
+            .set({
+              isUsed: true
+            })
+            .where(
+              and(
+                eq(schema.addresses.accountId, accountId),
+                eq(schema.addresses.isUsed, false),
+                inArray(schema.addresses.address, chunk)
+              )
+            )
+        }
+      })
+    }
 
     // Sort by timestamp descending (newest first)
     // Since listTransactions returns a batch of "recent" transactions,
@@ -42,14 +70,11 @@ export async function findTransactions(accountId: string, query: QueryValidator)
 }
 
 export async function findUTXOs(accountId: string) {
-  // 'use cache'
-  // cacheTag('utxos')
-  // cacheLife('seconds')
+  'use cache'
+  cacheTag('utxos')
+  cacheLife('seconds')
 
   try {
-    const cookieStore = await cookies()
-    await useAuth(cookieStore)
-
     const rpcClient = new RPCClient()
     await rpcClient.setWallet(accountId)
 
